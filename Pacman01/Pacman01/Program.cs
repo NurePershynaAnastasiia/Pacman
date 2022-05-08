94492a62d6f64248c2806bafc44f8926784e348c
Anastasiia Pershyna <anastasiia.pershyna@nure.ua>
2022-05-08 14:33:46 +0300

    Update
    - pacman moves (WASD)
    - dictionary with coords
    - levels load from txt files

--- Pacman01/Pacman01/Program.cs
+++ Pacman01/Pacman01/Program.cs
@@ -1,14 +1,66 @@
 ﻿using System;
 
+
 abstract class Element
 {
     abstract public void Draw();
 }
 class Pacman : Element
 {
+    private int x, y;
+    public bool lost;
+
+    public Pacman(int x, int y, bool lost = false)
+    {
+        this.x = x;
+        this.y = y;
+        this.lost = lost;
+    }
+    public int X
+    {
+        get { return x; }
+        set { x = value; }
+    }
+
+    public int Y
+    {
+        get { return y; }
+        set { y = value; }
+    }
+
+    public void Step(Element[,] Field, Dictionary<char, Coords> Coords_upd, char direction)
+    {
+        Field[x, y] = new Cell();
+        if (Field[x + Coords_upd[direction].x, y + Coords_upd[direction].y].ToString() == "Enemy")
+        {
+            Game_over();
+        }
+        else if (Field[x + Coords_upd[direction].x, y + Coords_upd[direction].y].ToString() != "Wall")
+        {
+            this.x = x + Coords_upd[direction].x;
+            this.y = y + Coords_upd[direction].y;
+        }
+
+
+    }
     public override void Draw()
     {
         Console.Write('o');
+    }
+
+    public void Game_over()
+    {
+        this.lost = true;
+        Console.Clear();
+        string path = "game_over.txt";
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
     }
 }
 class Wall : Element
@@ -34,6 +86,7 @@ class Cell : Element
 }
 class Enemy : Element
 {
+
     public override void Draw()
     {
         Console.Write('A');
@@ -47,48 +100,108 @@ class Energizer : Element
     }
 }
 
+public struct Coords
+{
+    public int x, y;
+
+    public Coords(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Element[,] Field = new Element[7, 13];
-
-
-        //filling the field
-        for (int i = 0; i < Field.GetLength(0); i++)
+        //Coords dictionary
+        Dictionary<char, Coords> Coords_upd = new Dictionary<char, Coords>();
+        Coords_upd.Add('u', new Coords(-1, 0));
+        Coords_upd.Add('d', new Coords(1, 0));
+        Coords_upd.Add('r', new Coords(0, 1));
+        Coords_upd.Add('l', new Coords(0, -1));
+        //Choosing level
+        int lvl = Choose_level();
+        int x, y, height, width;
+        switch (lvl)
         {
-            for (int j = 0; j < Field.GetLength(1); j++)
+            case 1:
+                height = 7;
+                width = 13;
+                x = 3;
+                y = 6;
+                break;
+            case 2:
+                height = 7;
+                width = 13;
+                x = 3;
+                y = 6;
+                break;
+            case 3:
+                height = 13;
+                width = 27;
+                x = 4;
+                y = 13;
+                break;
+            default:
+                height = 13;
+                width = 27;
+                x = 3;
+                y = 6;
+                break;
+
+        }
+
+        Element[,] Field = new Element[height, width];
+        Pacman pacman = new Pacman(x, y);
+
+        Start(Field, lvl);
+        Draw(Field);
+
+        ConsoleKeyInfo key_pressed;
+        key_pressed = Console.ReadKey();
+
+        while (true && !pacman.lost)
+        {
+            Update(Field, pacman);
+
+            key_pressed = (Console.KeyAvailable == false) ? key_pressed : Console.ReadKey();
+            //direction pacman
+            if (key_pressed.Key == ConsoleKey.W) pacman.Step(Field, Coords_upd, 'u');
+            if (key_pressed.Key == ConsoleKey.S) pacman.Step(Field, Coords_upd, 'd');
+            if (key_pressed.Key == ConsoleKey.A) pacman.Step(Field, Coords_upd, 'l');
+            if (key_pressed.Key == ConsoleKey.D) pacman.Step(Field, Coords_upd, 'r');
+
+        }
+    }
+
+    static int Choose_level()
+    {
+        Console.Clear();
+        string path = "choose_level.txt";
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
             {
-                Field[i, j] = new Wall();
+                Console.WriteLine(line);
             }
         }
-        for (int i = 1; i < Field.GetLength(0); i += 2)
-        {
-            for (int j = 1; j < Field.GetLength(1) - 1; j++)
-            {
-                Field[i, j] = new Coin();
-            }
-        }
-        for (int i = 1; i < Field.GetLength(0) - 1; i++)
-        {
-            Field[i, 1] = new Coin();
-            Field[i, 5] = new Coin();
-            Field[i, 7] = new Coin();
-            Field[i, 11] = new Coin();
-        }
-        Field[1, 6] = new Wall();
-        Field[5, 6] = new Wall();
+        int x = Console.Read();
+        if (x - '0' >= 1 && x - '0' <= 3) return x - '0';
+        else return 1;
 
-        Field[3, 6] = new Pacman();
-
-        Field[1, 9] = new Enemy();
-        Field[5, 1] = new Enemy();
-        Field[5, 5] = new Enemy();
-
-        Field[2, 1] = new Energizer();
-        Field[5, 11] = new Energizer();
-        //
-
+    }
+    static void Update(Element[,] Field, Pacman pacman)
+    {
+        Field[pacman.X, pacman.Y] = pacman;
+        Draw(Field);
+    }
+    static void Draw(Element[,] Field)
+    {
+        Console.Clear();
         for (int i = 0; i < Field.GetLength(0); i++)
         {
             for (int j = 0; j < Field.GetLength(1); j++)
@@ -97,7 +210,52 @@ class Program
             }
             Console.WriteLine();
         }
+        Thread.Sleep(500);
     }
+
+    static void Start(Element[,] Field, int lvl)
+    {
+        //filling the field 
+        string path;
+        switch (lvl)
+        {
+            case 1:
+                path = "lvl1.txt";
+                break;
+            case 2:
+                path = "lvl1.txt";
+                break;
+            case 3:
+                path = "lvl3.txt";
+                break;
+            default:
+                path = "lvl1.txt";
+                break;
+
+        }
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            int i = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                //Console.WriteLine(line);
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == '#') Field[i, j] = new Wall();
+                    if (line[j] == '.') Field[i, j] = new Coin();
+                    if (line[j] == ' ') Field[i, j] = new Cell();
+                    if (line[j] == '@') Field[i, j] = new Energizer();
+                    if (line[j] == 'A') Field[i, j] = new Enemy();
+                    if (line[j] == 'o') Field[i, j] = new Pacman(i, j);
+                }
+                i++;
+            }
+        }
+    }
+
+
 }
 
 
