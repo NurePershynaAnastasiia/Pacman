384ef4ef2e9f2d0ab468803ecc02ce6fffb4d765
asya488 <anastasiia.pershyna@nure.ua>
2022-06-12 01:00:21 +0300

    Created a new class EnemyStep and all the functions connected to enemies steps are moved to it

---
+++ Pacman01/Pacman01/EnemyStep.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    public class EnemyStep
+    {
+        public static bool CycleCheck(char dir, char prev)
+        {
+            if (prev == 'u' && dir == 'd') return false;
+            if (prev == 'd' && dir == 'u') return false;
+            if (prev == 'l' && dir == 'r') return false;
+            if (prev == 'r' && dir == 'l') return false;
+            return true;
+        }
+
+        public static char RandomDir(Elements.Field field, Elements.Enemy enemy)
+        {
+            char direction = 'u';
+            Random rnd = new Random();
+            int dir = rnd.Next();
+
+            while (field[enemy.X + Utilities.Utility.CoordsUpdate(direction).x, enemy.Y + Utilities.Utility.CoordsUpdate(direction).y].isObstacle() || !CycleCheck(direction, enemy.Prev))
+            {
+                dir++;
+                switch (dir % 4)
+                {
+                    case 0:
+                        direction = 'u';
+                        break;
+                    case 1:
+                        direction = 'd';
+                        break;
+                    case 2:
+                        direction = 'r';
+                        break;
+                    case 3:
+                        direction = 'l';
+                        break;
+                }
+
+            }
+            enemy.Prev = direction;
+            return direction;
+        }
+
+        public static void EnemyStatus(Elements.Enemy enemy, Elements.Field field)
+        {
+            if (enemy.Eaten)
+                enemy.TimeEaten++;
+            if (enemy.TimeEaten == 20)
+            {
+                enemy.Eaten = false;
+                enemy.TimeEaten = 0;
+            }
+
+            if (field.Scared)
+                enemy.Scared = true;
+            else
+                enemy.Scared = false;
+
+            if (field[enemy.X, enemy.Y] is Elements.Pacman && field.Scared)
+                enemy.Eaten = true;
+
+        }
+
+        public static void StepEnemy(Elements.Enemy enemy, Elements.Field field, Elements.Field fieldEnemies, char dir, int lvl)
+        {
+            EnemyStatus(enemy, field);
+
+            if (!(field[enemy.X, enemy.Y] is Elements.Pacman))
+            {
+                Console.SetCursorPosition(enemy.Y, enemy.X);
+                enemy.Draw();
+            }
+
+            if (!enemy.Eaten)
+            {
+                Console.SetCursorPosition(enemy.Y, enemy.X);
+                field[enemy.X, enemy.Y].Draw();
+                Console.SetCursorPosition(enemy.Y + Utilities.Utility.CoordsUpdate(dir).y, enemy.X + Utilities.Utility.CoordsUpdate(dir).x);
+                enemy.Draw();
+
+                if (field[enemy.X + Utilities.Utility.CoordsUpdate(dir).x, enemy.Y + Utilities.Utility.CoordsUpdate(dir).y] is Elements.Pacman && !field.Scared)
+                    field.GameOver = true;
+                if (field[enemy.X + Utilities.Utility.CoordsUpdate(dir).x, enemy.Y + Utilities.Utility.CoordsUpdate(dir).y] is Elements.Pacman && field.Scared)
+                    enemy.Eaten = true;
+
+
+                fieldEnemies[enemy.X, enemy.Y] = new Elements.Cell();
+                enemy.X += Utilities.Utility.CoordsUpdate(dir).x;
+                enemy.Y += Utilities.Utility.CoordsUpdate(dir).y;
+                fieldEnemies[enemy.X, enemy.Y] = enemy;
+            }
+        }
+    }
+}
--- Pacman01/Pacman01/Program.cs
+++ Pacman01/Pacman01/Program.cs
@@ -43,7 +43,7 @@ namespace Pacman
                     }
                     for (int i = 0; i < Utilities.Utility.LevelInfo(lvl).numberOfEnemies; i++) //all the enemies make their step
                     {
-                        StepEnemy(enemies[i], field, fieldEnemies, RandomDir(fieldEnemies, enemies[i]), lvl);
+                        EnemyStep.StepEnemy(enemies[i], field, fieldEnemies, EnemyStep.RandomDir(fieldEnemies, enemies[i]), lvl);
                         if (field.GameOver)
                         {
                             Interface.Game_over();
@@ -120,95 +120,6 @@ namespace Pacman
 
               return chosenDesign;
         }
-        public static bool CycleCheck(char dir, char prev)
-        {
-            if (prev == 'u' && dir == 'd') return false;
-            if (prev == 'd' && dir == 'u') return false;
-            if (prev == 'l' && dir == 'r') return false;
-            if (prev == 'r' && dir == 'l') return false;
-            return true;
-        }
-
-        public static char RandomDir(Elements.Field field, Elements.Enemy enemy)
-        {
-            char direction = 'u';
-            Random rnd = new Random();
-            int dir = rnd.Next();
-
-            while (field[enemy.X + Utilities.Utility.CoordsUpdate(direction).x, enemy.Y + Utilities.Utility.CoordsUpdate(direction).y].isObstacle() || !CycleCheck(direction, enemy.Prev))
-            {
-                dir++;
-                switch (dir % 4)
-                {
-                    case 0:
-                        direction = 'u';
-                        break;
-                    case 1:
-                        direction = 'd';
-                        break;
-                    case 2:
-                        direction = 'r';
-                        break;
-                    case 3:
-                        direction = 'l';
-                        break;
-                }
-
-            }
-            enemy.Prev = direction;
-            return direction;
-        }
-
-       public static void EnemyStatus(Elements.Enemy enemy, Elements.Field field)
-        {
-            if (enemy.Eaten)
-                enemy.TimeEaten++;
-            if (enemy.TimeEaten == 20)
-            {
-                enemy.Eaten = false;
-                enemy.TimeEaten = 0;
-            }
-
-            if (field.Scared)
-                enemy.Scared = true;
-            else
-                enemy.Scared = false;
-
-            if (field[enemy.X, enemy.Y] is Elements.Pacman && field.Scared)
-                enemy.Eaten = true;
-
-        }
-
-        public static void StepEnemy(Elements.Enemy enemy, Elements.Field field, Elements.Field fieldEnemies, char dir, int lvl)
-        {
-           EnemyStatus(enemy, field);
-
-            if (!(field[enemy.X, enemy.Y] is Elements.Pacman))
-            {
-                Console.SetCursorPosition(enemy.Y, enemy.X);
-                enemy.Draw();
-            }
-
-            if (!enemy.Eaten)
-            {    
-                Console.SetCursorPosition(enemy.Y, enemy.X);
-                field[enemy.X, enemy.Y].Draw();
-                Console.SetCursorPosition(enemy.Y + Utilities.Utility.CoordsUpdate(dir).y, enemy.X + Utilities.Utility.CoordsUpdate(dir).x);
-                enemy.Draw();
-
-               if (field[enemy.X + Utilities.Utility.CoordsUpdate(dir).x, enemy.Y + Utilities.Utility.CoordsUpdate(dir).y] is Elements.Pacman && !field.Scared)
-                   field.GameOver = true;
-               if (field[enemy.X + Utilities.Utility.CoordsUpdate(dir).x, enemy.Y + Utilities.Utility.CoordsUpdate(dir).y] is Elements.Pacman && field.Scared)
-                   enemy.Eaten = true;
-
-
-                fieldEnemies[enemy.X, enemy.Y] = new Elements.Cell();
-                enemy.X += Utilities.Utility.CoordsUpdate(dir).x;
-                enemy.Y += Utilities.Utility.CoordsUpdate(dir).y;
-                fieldEnemies[enemy.X, enemy.Y] = enemy;
-            }
-        }
-
 
         public static void Start(Pacman.Elements.Field field, Pacman.Elements.Field fieldEnemies, string path, Elements.Enemy[] enemies, int lvl)
         {
